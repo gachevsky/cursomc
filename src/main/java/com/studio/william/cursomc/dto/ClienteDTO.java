bb107b32fff0cdf1a7f8dbce395f495aa38b056e
William <wospino@live.com>
2021-05-25 21:28:04 -0500

    Validacion personalida, email no repetido en la actualizacion del cliente

--- src/main/java/com/studio/william/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/studio/william/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.studio.william.cursomc.domain.Cliente;
+import com.studio.william.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	
